f48a216faf5d7b56ca95fc3e20d3b2eaf75268db
Alexander <sashakud0598@yandex.ru>
2020-06-25 19:28:57 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AVLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> tree = new AVLTree<int>();
+            tree.Add(12);
+            tree.Add(5);
+            tree.Add(6);
+            tree.Add(7);
+            tree.Add(3);
+                        
+            foreach (var item in tree)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            
+            tree.Remove(6);
+            foreach (var item in tree)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+        }
+    }
+}
